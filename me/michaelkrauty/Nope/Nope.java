1274a6545145df44a2c8f410de47df2e0bbea3df
michaelkrauty <michaelkrauty@gmail.com>
2013-08-17 10:40:44 -0700

    1

---
+++ me/michaelkrauty/Nope/Nope.java
@@ -0,0 +1,19 @@
+package me.michaelkrauty.Nope;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Nope extends JavaPlugin {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		Player player = (Player) sender;
+		if(commandLabel.equalsIgnoreCase("nope"))	
+		player.kickPlayer("Nope.");
+		
+		return true;
+	}
+
+}
