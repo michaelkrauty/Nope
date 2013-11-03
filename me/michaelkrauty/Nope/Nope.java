6017fa39fc5312f294e7c8540dc62daea1d20abc
michaelkrauty <michaelkrauty@gmail.com>
2013-11-03 09:29:30 -0800

    1

--- me/michaelkrauty/Nope/Nope.java
+++ me/michaelkrauty/Nope/Nope.java
@@ -8,11 +8,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Nope extends JavaPlugin {
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		Player player = (Player) sender;
+		Player player = this.getServer().getPlayer(args[0]);
 		if(commandLabel.equalsIgnoreCase("nope"))	
+			player.chat("Nope.");
+		try{
+			Thread.sleep(100);
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
 		player.kickPlayer("Nope.");
-		
 		return true;
 	}
-
+	
 }
